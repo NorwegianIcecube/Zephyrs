bfab032014fa4d39df09ecc7e48d840946152398
Ask Sødal <asks@uia.no>
2023-03-07 01:28:26 +0100

    now gets keypair from secret key, and should display account balance

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,15 @@
-import * as solanaWeb3 from "https://unpkg.com/@solana/web3.js@latest/lib/index.iife.js";
-var keypair = null;
-function changeTabTo(tabId) {
+// ¤ state -===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-
+let keypair_user = null;
+let keypair_program = solanaWeb3.Keypair.generate();
+import { endpoint } from "./config.js";
+let connection = null;
+// About Connection():
+//  if you want to use solana-test-validator, in my case, I use endpoint = http://localhost:8899 in config.json
+//  if you want to use solana dev net, use solanaWeb3.clusterApiUrl("devnet"), or https://api.devnet.solana.com?
 
+// ¤ functions -===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-
+function changeTabTo(tabId) {
+    // todo: use querySelector instead
     var tabs = document.getElementById("tabs").children;
     var tab_content = document.getElementById("tab-content").children;
 
@@ -21,37 +29,37 @@ function changeTabTo(tabId) {
     }
 }
 
-
-/// check for public key given private key
-function trySignIn(private_key) {
-
-    console.warn("this function is not working yet, try setting parametre as \"secret key\"");
-    try {
-        keypair = solanaWeb3.Keypair.fromSecretKey(private_key);
-    } catch (e) {
-        console.error(e);
-    }
-    if (private_key == "secret key") {
-        return true;
-    } else { return false; }
-}
-
-var input = document.querySelector("#secret-key");;
-input.addEventListener("keypress", function (event) {
-    if (event.key == "Enter") {
-        var status = trySignIn(input.value);
-        if (status == true) {
-            document.querySelector("#not-signed-in").style.display = "none";
-            document.querySelector("#signed-in").style.display = "block";
-        } else {
-            input.placeholder = "invalid key, try again";
-            input.value = "";
-        }
-        ;
-    }
-});
-
+// ¤ run -===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-
 var tabs = document.querySelectorAll("#tabs>button");
 tabs[0].onclick = function () { changeTabTo(1) };
 tabs[1].onclick = function () { changeTabTo(2) };
 tabs[2].onclick = function () { changeTabTo(3) };
+
+var input = document.querySelector("#secret-key");
+input.placeholder = "[1, 2, 3, ...]";
+input.addEventListener("keypress", function (event) {
+    if (event.key == "Enter") {
+        try {
+            connection = new solanaWeb3.Connection(endpoint);
+        } catch (e) {
+            console.error("couldn't find network at endpoint: ", endpoint, ", see error: ", e);
+            return;
+        } try {
+            keypair_user = solanaWeb3.Keypair.fromSecretKey(Uint8Array.from(JSON.parse(input.value)));
+        } catch (e) {
+            console.warn("invalid secret key, see error: ", e);
+            input.placeholder = "invalid key, try again";
+            input.value = "";
+            return;
+        }
+
+        connection.getBalance(keypair_user.publicKey).then(balance => {
+            document.querySelector("h1").innerHTML = "welcome!";
+            document.querySelector("#balance").innerHTML = "balance: " + balance + " SOL";
+            document.querySelector("#not-signed-in").style.display = "none";
+            document.querySelector("#signed-in").style.display = "block";
+        }).catch(err => {
+            console.error("couldn't connect to blockchain, see error: ", err);
+        });
+    }
+});
